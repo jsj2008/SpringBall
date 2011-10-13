0fc6d7c59e1eb008564a5633e4a420ebf7c76d51
Zulus <vgzulus@gmail.com>
2011-10-13 07:35:08 +0600

    menu2

---
+++ SpringBall/SeasonLayer.h
@@ -0,0 +1,13 @@
+//
+//  SeasonLayer.h
+//  BestJumps
+//
+//  Created by вадим on 10/13/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SeasonLayer : NSObject
+
+@end
---
+++ SpringBall/SeasonLayer.m
@@ -0,0 +1,23 @@
+//
+//  SeasonLayer.m
+//  BestJumps
+//
+//  Created by вадим on 10/13/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "SeasonLayer.h"
+
+@implementation SeasonLayer
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
