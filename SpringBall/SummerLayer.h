bc64d16a5b721f37ca594dcf51e970c307dc076f
zulus128 <vgzulus@gmail.com>
2011-10-13 09:29:12 +0300

    fff

---
+++ SpringBall/SummerLayer.h
@@ -0,0 +1,16 @@
+//
+//  SummerLayer.h
+//  BestJumps
+//
+//  Created by naceka on 13.10.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface SummerLayer : NSObject {
+    
+}
+
+@end
---
+++ SpringBall/SummerLayer.m
@@ -0,0 +1,14 @@
+//
+//  SummerLayer.m
+//  BestJumps
+//
+//  Created by naceka on 13.10.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "SummerLayer.h"
+
+
+@implementation SummerLayer
+
+@end
